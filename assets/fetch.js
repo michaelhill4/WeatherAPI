ee638a388265dd07e21e17fbfd657653c94141da
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-08-17 20:40:45 -0400

    added card stuff

--- assets/fetch.js
+++
@@ -1,8 +0,0 @@
-class UI() {
-    this.uiContainer= document.getElementById("content");
-    this.city;
-}
-
-populateUI(data) {
-    this.uiContainer.innerHTML = 
-}
--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,32 @@
 console.log("Hello World")
 
+var weatherApiKey = "5768025933467807bb6d8606bf557031"
+var weatherCard = document.getElementById("content")
+
+
 document.getElementById("submit").addEventListener("click", function () {
     console.log(document.getElementById("searchUser").value)
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="&appid=5768025933467807bb6d8606bf557031").then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-        console.log(response)
-        return response.json();
-    })
-    
-    .then(function (weatherData) {
-        console.log(weatherData.city.name)
-    })
-}
+    var cityName = document.getElementById("searchUser").value
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${weatherApiKey}`)
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            console.log(response)
+            return response.json();
+        })
+
+        .then(function (weatherData) {
+            console.log(weatherData)
+            console.log(weatherData.city.name)
+
+            document.getElementById("tempInput").textContent = weatherData.list[0].main.temp
+
+            // document.getElementbyId = navigate into data array
+        })
+})
+
+
 
 // 5768025933467807bb6d8606bf557031
-"https://api.openweathermap.org/data/2.5/forecast?q=sacramento&appid=5768025933467807bb6d8606bf557031"
-
-var franklin = 42
-
-"start" + franklin + "end of sentence"
-
-`start ${franklin} end of sentence`
+// "https://api.openweathermap.org/data/2.5/forecast?q=sacramento&appid=5768025933467807bb6d8606bf557031"
